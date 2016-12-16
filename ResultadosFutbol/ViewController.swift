ccacf9a499b79bb0b698f89285cfe069a9cdbf8d
Ignacio Barrios <i.barriosp@outlook.es>
2016-12-16 17:30:44 +0100

    Initial Commit

---
+++ ResultadosFutbol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResultadosFutbol
+//
+//  Created by Nacho on 16/12/16.
+//  Copyright © 2016 UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
